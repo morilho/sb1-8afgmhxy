b795b29d216f862c1a53fd0d1e37ba9ef901fb6e
morilho <morilho@gmail.com>
2025-03-26 10:59:56 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -79,7 +79,7 @@ export default function App() {
       <div 
         className="absolute inset-0 w-full h-full bg-cover bg-center bg-no-repeat"
         style={{
-          backgroundImage: `url('/frozen-background.jpg')`,
+          backgroundImage: `url('/frozen-background.png')`,
           backgroundPosition: 'center 100%'
         }}
       >
